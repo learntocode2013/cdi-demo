dd644e563d4e50c6b0e39e600cc1243b7a0fbc44
disen <disen@cisco.com>
2016-03-30 07:30:22 +0530

    New maven project created for CDI

---
+++ cdi-demo/src/main/java/org/javaee7/cdi/App.java
@@ -0,0 +1,13 @@
+package org.javaee7.cdi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
